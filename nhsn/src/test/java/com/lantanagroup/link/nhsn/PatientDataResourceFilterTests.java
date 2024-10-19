fa9c8a0f325d20d420a7846de164b74b4407e5e8
Austin Moody <amoody@ainq.com>
2024-10-19 19:20:48 -0400

    Correct unit tests broken by adding organization id into the mix when generating a report.

--- nhsn/src/test/java/com/lantanagroup/link/nhsn/PatientDataResourceFilterTests.java
+++ nhsn/src/test/java/com/lantanagroup/link/nhsn/PatientDataResourceFilterTests.java
@@ -14,8 +14,8 @@ public class PatientDataResourceFilterTests {
   /**
    * Condition
    * Uses onsetPeriod
-   * Has neither start or end
-   * Should remove because we can't determine and it would likely be excluded by measures anyways
+   * Has neither start nor end
+   * Should remove because we can't determine, and it would likely be excluded by measures anyway
    */
   @Test
   public void shouldRemove_Condition_PeriodNoStartNoEnd_Remove() {
@@ -23,7 +23,7 @@ public class PatientDataResourceFilterTests {
     Condition condition = new Condition();
     condition.setOnset(period);
 
-    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
+    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "","2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
     boolean actual = PatientDataResourceFilter.shouldRemove(criteria, java.time.Period.ofDays(90), condition);
     Assert.assertTrue(actual);
   }
@@ -31,9 +31,8 @@ public class PatientDataResourceFilterTests {
   /**
    * Condition
    * Uses onsetPeriod
-   * Only has start
-   * Start is 1/1/22 @ 5:40
-   * Report period is 1/1/22 - 1/31/22 with a 90d lookback (starting at 10/3)
+   * Only has start: 1/1/22 @ 5:40
+   * Report period is 1/1/22 - 1/31/22 with a 90d look back (starting at 10/3)
    * should NOT remove
    */
   @Test
@@ -42,7 +41,7 @@ public class PatientDataResourceFilterTests {
     Condition condition = new Condition();
     condition.setOnset(period);
 
-    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
+    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "", "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
     boolean actual = PatientDataResourceFilter.shouldRemove(criteria, java.time.Period.ofDays(90), condition);
     Assert.assertFalse(actual);
   }
@@ -50,9 +49,8 @@ public class PatientDataResourceFilterTests {
   /**
    * Condition
    * Uses onsetPeriod
-   * Only has start
-   * Start is 9/5/22 @ 5:40
-   * Report period is 1/1/22 - 1/31/22 with a 90d lookback (starting at 10/3/21)
+   * Only has start 9/5/22 @ 5:40
+   * Report period is 1/1/22 - 1/31/22 with a 90d look back (starting at 10/3/21)
    * SHOULD remove
    */
   @Test
@@ -61,7 +59,7 @@ public class PatientDataResourceFilterTests {
     Condition condition = new Condition();
     condition.setOnset(period);
 
-    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
+    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "", "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
     boolean actual = PatientDataResourceFilter.shouldRemove(criteria, java.time.Period.ofDays(90), condition);
     Assert.assertTrue(actual);
   }
@@ -69,9 +67,8 @@ public class PatientDataResourceFilterTests {
   /**
    * Condition
    * Uses onsetPeriod
-   * Only has end
-   * End is 1/1/22 @ 5:40
-   * Report period is 1/1/22 - 1/31/22 with a 90d lookback (starting at 10/3)
+   * Only has end 1/1/22 @ 5:40
+   * Report period is 1/1/22 - 1/31/22 with a 90d look back (starting at 10/3)
    * should NOT remove
    */
   @Test
@@ -80,7 +77,7 @@ public class PatientDataResourceFilterTests {
     Condition condition = new Condition();
     condition.setOnset(period);
 
-    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
+    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "", "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
     boolean actual = PatientDataResourceFilter.shouldRemove(criteria, java.time.Period.ofDays(90), condition);
     Assert.assertFalse(actual);
   }
@@ -88,9 +85,8 @@ public class PatientDataResourceFilterTests {
   /**
    * Condition
    * Uses onsetPeriod
-   * Only has end
-   * End is 9/5/22 @ 5:40
-   * Report period is 1/1/22 - 1/31/22 with a 90d lookback (starting at 10/3/21)
+   * Only has end 9/5/22 @ 5:40
+   * Report period is 1/1/22 - 1/31/22 with a 90d look back (starting at 10/3/21)
    * SHOULD remove
    */
   @Test
@@ -99,7 +95,7 @@ public class PatientDataResourceFilterTests {
     Condition condition = new Condition();
     condition.setOnset(period);
 
-    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
+    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "", "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
     boolean actual = PatientDataResourceFilter.shouldRemove(criteria, java.time.Period.ofDays(90), condition);
     Assert.assertTrue(actual);
   }
@@ -109,8 +105,8 @@ public class PatientDataResourceFilterTests {
    * Uses onsetPeriod
    * Has both start ane end
    * 1/29/22 @ 5:40 - 2/3/22 @ 7:30
-   * Report period is 1/1/22 - 1/31/22 with a 90d lookback (starting at 10/3)
-   * should NOT remove because period.start overlaps
+   * Report period is 1/1/22 - 1/31/22 with a 90d look back (starting at 10/3)
+   * should NOT remove because "period.start" overlaps
    */
   @Test
   public void shouldRemove_Condition_PeriodStartAndEnd_DontRemove() {
@@ -120,7 +116,7 @@ public class PatientDataResourceFilterTests {
     Condition condition = new Condition();
     condition.setOnset(period);
 
-    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
+    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "", "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
     boolean actual = PatientDataResourceFilter.shouldRemove(criteria, java.time.Period.ofDays(90), condition);
     Assert.assertFalse(actual);
   }
@@ -130,7 +126,7 @@ public class PatientDataResourceFilterTests {
    * Uses onsetPeriod
    * Has both start ane end
    * 09/08/21 @ 5:40 - 09/12/21 @ 7:30
-   * Report period is 1/1/22 - 1/31/22 with a 90d lookback (starting at 10/3/21)
+   * Report period is 1/1/22 - 1/31/22 with a 90d look back (starting at 10/3/21)
    * SHOULD remove
    */
   @Test
@@ -141,7 +137,7 @@ public class PatientDataResourceFilterTests {
     Condition condition = new Condition();
     condition.setOnset(period);
 
-    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
+    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "", "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
     boolean actual = PatientDataResourceFilter.shouldRemove(criteria, java.time.Period.ofDays(90), condition);
     Assert.assertTrue(actual);
   }
@@ -149,7 +145,7 @@ public class PatientDataResourceFilterTests {
   /**
    * ServiceRequest
    * Uses authoredOn of 1/8/22 @ 5:40
-   * Report period is 1/1/22 - 1/31/22 with a 90d lookback (starting at 10/3/21)
+   * Report period is 1/1/22 - 1/31/22 with a 90d look back (starting at 10/3/21)
    * should NOT remove
    */
   @Test
@@ -157,7 +153,7 @@ public class PatientDataResourceFilterTests {
     ServiceRequest serviceRequest = new ServiceRequest();
     serviceRequest.setAuthoredOnElement(new DateTimeType("2022-01-08T05:40:00.000+00:00"));
 
-    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
+    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "", "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
     boolean actual = PatientDataResourceFilter.shouldRemove(criteria, java.time.Period.ofDays(90), serviceRequest);
     Assert.assertFalse(actual);
   }
@@ -165,7 +161,7 @@ public class PatientDataResourceFilterTests {
   /**
    * ServiceRequest
    * Uses authoredOn of 9/3/21 @ 5:40
-   * Report period is 1/1/22 - 1/31/22 with a 90d lookback (starting at 10/3/21)
+   * Report period is 1/1/22 - 1/31/22 with a 90d look back (starting at 10/3/21)
    * SHOULD remove
    */
   @Test
@@ -173,7 +169,7 @@ public class PatientDataResourceFilterTests {
     ServiceRequest serviceRequest = new ServiceRequest();
     serviceRequest.setAuthoredOnElement(new DateTimeType("2021-09-03T05:40:00.000+00:00"));
 
-    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
+    ReportCriteria criteria = new ReportCriteria(new ArrayList<>(), "", "2022-01-01T00:00:00.000+00:00", "2022-01-31T23:59:59.000+00:00");
     boolean actual = PatientDataResourceFilter.shouldRemove(criteria, java.time.Period.ofDays(90), serviceRequest);
     Assert.assertTrue(actual);
   }
